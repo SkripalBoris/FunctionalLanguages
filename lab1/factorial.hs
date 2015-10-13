094ec21e7cf312e06fdb49d5536cc5205aef04ad
SkripalBoris <skipalboris@gmail.com>
2015-10-14 02:08:55 +0300

    add interact

--- lab1/factorial.hs
+++ lab1/factorial.hs
@@ -1,8 +1,7 @@
 fact :: Int -> Int
 fact x = if x <= 1 then 1 else x * fact (x - 1)
 
-main = do
-	print ("Please enter a number")
-	x <- getLine 
-	print ("Factorial is ")
-	print ( show ( fact ( read x ) ) )
+factString :: String -> String
+factString str = "Answer: " ++ show (fact (read (head (lines str)))) ++ "\n"
+
+main = interact factString
